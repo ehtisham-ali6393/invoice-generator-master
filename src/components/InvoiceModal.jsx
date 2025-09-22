448deda63541e780b3a2feaa54df1ed89673d8fe
Ehtisham Ali <ehtishamofficial23@gmail.com>
2025-09-22 23:00:03 +0530

    invoice update

--- src/components/InvoiceModal.jsx
+++ src/components/InvoiceModal.jsx
@@ -128,7 +128,7 @@ const InvoiceModal = ({
                   {/* Invoice meta */}
                   <div className="p-1 pl-2 pr-2 border border-black/50 border-b-0 text-[9px] grid grid-cols-3">
                     <div className='flex '>
-                      <span className="font-bold">Invoice No : </span>
+                      <span className="font-bold">Invoice No</span>
                       <span className='pl-1'> {invoiceInfo.invoiceNumber} </span>
                     </div>
                     <div className='flex justify-center'>
@@ -137,7 +137,7 @@ const InvoiceModal = ({
                     </div>
 
                     <div className="flex justify-end">
-                      <span className="font-bold">Date : </span>
+                      <span className="font-bold">Date</span>
                       <span className='pl-1'>{invoiceInfo.today}</span>
                     </div>
                   </div>
